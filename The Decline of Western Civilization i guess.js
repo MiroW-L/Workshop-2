91cb6eeb7c9bda246cd7c286f2d74ad3ca3f3d4f
MiroW-L <mirowrightsonlyons@gmail.com>
2025-01-21 11:40:44 +1300

    Add files via upload

---
+++ The Decline of Western Civilization i guess.js
@@ -0,0 +1,46 @@
+let img; 
+
+function preload(){
+  img = loadImage('images/Minutemen.png');
+}
+
+let counter = 0;
+let countInterval = setInterval(makeBlood,4000);
+
+function setup () {
+createCanvas (600, 600); 
+background (220) ;
+setTimeout (makeBlood, 2000, random(0,width), random(0,height), 30, color(255,1,0));
+// setTimeout (makeBlood, 2000, random(0,width), random(0,height), 30, color(255,1,0));
+ 
+image(img,-250,-70);
+img.resize(1,1);
+}
+
+// function draw(){
+ 
+ 
+// }
+
+function makeBlood(xPos, yPos, size, colour) {
+  push();
+  fill(colour);
+  noStroke () ;
+  circle(xPos, yPos, size);
+  triangle(xPos+13, yPos-4, xPos-14, yPos+7, xPos-4, yPos-91);
+  //  circle(xPos-20, yPos-40, size-12);
+  //  circle(xPos, yPos+24, size-19);
+  counter++;
+    setTimeout (makeBlood, 2000, random(0,width), random(0,height), 30, color(255,0,0));
+    pop();
+  if (counter > 5){
+clearInterval (countInterval);
+
+}
+    
+function mousePressed () {
+set.Interval (makeBlood, 1000);
+  
+}
+
+}
